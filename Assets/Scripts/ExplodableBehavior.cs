35dd93f6dd3d991cfbf2225df6c9b60dd9e9ac90
Eduardo Vieira <eduardovra@gmail.com>
2015-03-01 20:20:42 -0300

    Animate block destroction

--- Assets/Scripts/ExplodableBehavior.cs
+++ Assets/Scripts/ExplodableBehavior.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ExplodableBehavior : MonoBehaviour {
 	public void Explode() {
-		Destroy (gameObject);
+		rigidbody.isKinematic = false;
+		rigidbody.AddForce(Vector3.up * 40, ForceMode.Impulse);
+		Destroy (gameObject, 3.0f);
 	}
 }
