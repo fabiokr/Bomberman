4312846c9a2599f9a3b3125db58f41c9a3ef279e
Fabio Kreusch <fabiokr@gmail.com>
2015-03-04 20:10:51 -0300

    Destroy item on touch

---
+++ Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBehavior : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.GetComponent<BombermanBehavior> ()) {
+			Destroy(gameObject);
+		}
+	}
+}
