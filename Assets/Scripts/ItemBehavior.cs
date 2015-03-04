4cac7e3f15228e8c6f6262f2950149a18ade5713
Fabio Kreusch <fabiokr@gmail.com>
2015-03-04 20:36:33 -0300

    Add ItemBombBehavior

--- Assets/Scripts/ItemBehavior.cs
+++ Assets/Scripts/ItemBehavior.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class ItemBehavior : MonoBehaviour {
-	
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.GetComponent<BombermanBehavior> ()) {
-			Destroy(gameObject);
+	bool activated = false;
+
+	protected BombermanBehavior GetBombermanBehavior(Collider other) {
+		BombermanBehavior b = other.gameObject.GetComponent<BombermanBehavior> ();
+
+		// only return the Behavior once, otherwise if there are multiple
+		// colliders they will all trigger the collider event
+		if (b && !activated) {
+			activated = true;
+			Destroy (gameObject);
+			return b;
+		} else {
+			return null;
 		}
 	}
 }
