06b2d51d8caf9b09a6be1e870c4c1b379cc2d42e
Fabio Kreusch <fabiokr@gmail.com>
2015-03-12 22:08:07 -0300

    Adds the timer

---
+++ Assets/Scripts/GameControllerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerBehavior : MonoBehaviour {
+	public static GameControllerBehavior instance { get; private set; }
+
+	public int startingTimer = 180;
+
+	bool timerHurry = false, timerFinished = false;
+	
+	void Start () {
+		instance = this;
+	}
+
+	public void TimerHurry() {
+		if (!timerHurry) {
+			timerHurry = true;
+			Debug.Log("HURRY!");
+		}
+	}
+
+	public void TimerFinished() {
+		if (!timerFinished) {
+			timerFinished = true;
+			Debug.Log("GAME OVER!");
+		}
+	}
+}
---
+++ Assets/Scripts/TimerBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerBehavior : MonoBehaviour {	
+	public float timer = 180;
+
+	Text text;
+	bool active = true;
+
+	void Start() {
+		text = GetComponent<Text> ();
+		timer = GameControllerBehavior.instance.startingTimer;
+	}
+
+	void Update () {
+		if (active) {
+			UpdateTimer();
+
+			int displaySeconds = (int)(timer % 60);
+
+			if (displaySeconds == 60) {
+				displaySeconds = 0;
+			}
+
+			int displayMinutes = (int)Mathf.Floor (timer / 60);
+
+			text.text = string.Format ("{0:00}:{1:00}", displayMinutes, displaySeconds);
+		}
+	}
+
+	void UpdateTimer() {
+		timer -= Time.deltaTime;
+
+		if ((int)timer == 60) {
+			GameControllerBehavior.instance.TimerHurry();
+		} else if ((int)timer == 0) {
+			active = false;
+			GameControllerBehavior.instance.TimerFinished();
+		}
+	}
+}
