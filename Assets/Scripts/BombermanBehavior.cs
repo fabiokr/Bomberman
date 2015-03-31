d5fc7cab609d0293d8fadd9ce684ce06bbe9d6d3
Eduardo Vieira <eduardovra@gmail.com>
2015-03-30 22:14:05 -0300

    Commit unity project

--- Assets/Scripts/BombermanBehavior.cs
+++ Assets/Scripts/BombermanBehavior.cs
@@ -86,7 +86,6 @@ public class BombermanBehavior : MonoBehaviour, ExplodableInterface {
 	}
 
 	public void PlayCatchItemSound () {
-		Debug.Log ("Playing ItemCatch sound");
 		audio.PlayOneShot (catchItem, 0.7f);
 	}
 
