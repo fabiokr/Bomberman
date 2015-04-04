86a47553d637798a82f42b30bc869aea790566c5
Eduardo Vieira <eduardovra@gmail.com>
2015-04-04 17:52:17 -0300

    Configure controls scene

---
+++ Assets/Scripts/ConfigureScene/ConfigureGameBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigureGameBehaviour : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void OnGUI () {
+		Event e = Event.current;
+		if (e.isKey)
+			Debug.Log("Detected key code: " + e.keyCode);
+	}
+
+	public void SetUpButton() {
+
+	}
+}
