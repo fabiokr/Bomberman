d0ee3368b4d4f7bcd7540fdf0e6ba897cffcc626
Eduardo Vieira <eduardovra@gmail.com>
2015-04-19 11:44:19 -0300

    Number of players are now selectable

--- Assets/Scripts/StartScene/StartGameBehavior.cs
+++ Assets/Scripts/StartScene/StartGameBehavior.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class StartGameBehavior : MonoBehaviour {
+	void Start() {
+		Text players = GameObject.Find ("Players").GetComponent<Text> ();
+		SetPlayers (players.text);
+	}
+
 	void Update() {
 		if (Input.GetKey (KeyCode.Return)) {
 			StartGame();
@@ -11,5 +17,9 @@ public class StartGameBehavior : MonoBehaviour {
 	public void StartGame() {
 		Debug.Log ("Starting Game");
 		Application.LoadLevel ("Arena"); 
+	}
+
+	public void SetPlayers (string players) {
+		PlayerPrefs.SetInt ("Players", int.Parse (players));
 	}
 }
